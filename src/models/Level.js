acc0806801f640014d74d17460b793ffb34d935c
Peter <p3732@users.noreply.github.com>
2020-01-31 22:03:27 +0100

    models: Update Level definition

--- src/models/Level.js
+++ src/models/Level.js
@@ -3,18 +3,20 @@ module.exports = function(sequelize, DataTypes) {
    * A ecce_mono level.
    */
   var Level = sequelize.define("Level", {
-    image_uri: {type: DataTypes.TEXT, allowNull: false},
-    x1: {type: DataTypes.INTEGER, allowNull: false},
-    x2: {type: DataTypes.INTEGER, allowNull: false},
-    y1: {type: DataTypes.INTEGER, allowNull: false},
-    y2: {type: DataTypes.INTEGER, allowNull: false},
-    overlay_uri: {type: DataTypes.TEXT, allowNull: false},
-    time: {type: DataTypes.INTEGER, allowNull: false},
-    hex_value_1: {type: DataTypes.INTEGER, allowNull: false},
-    hex_value_2: {type: DataTypes.INTEGER, allowNull: false},
-    hex_value_3: {type: DataTypes.INTEGER, allowNull: false},
-    hex_value_4: {type: DataTypes.INTEGER, allowNull: false},
-    hex_value_5: {type: DataTypes.INTEGER, allowNull: false}
+    title: {type: DataTypes.TEXT, allowNull: false},
+    image: {type: DataTypes.TEXT, allowNull: false},
+    overlay: {type: DataTypes.TEXT, allowNull: false},
+    //upper left corner coordinates of overlay
+    x: {type: DataTypes.INTEGER, allowNull: false},
+    y: {type: DataTypes.INTEGER, allowNull: false},
+    width: {type: DataTypes.INTEGER, allowNull: false},
+    height: {type: DataTypes.INTEGER, allowNull: false},
+    timeout: {type: DataTypes.INTEGER, allowNull: false},
+    hex_value_1: {type: DataTypes.TEXT, allowNull: false},
+    hex_value_2: {type: DataTypes.TEXT, allowNull: true},
+    hex_value_3: {type: DataTypes.TEXT, allowNull: true},
+    hex_value_4: {type: DataTypes.TEXT, allowNull: true},
+    hex_value_5: {type: DataTypes.TEXT, allowNull: true}
   });
 
   return Level;
