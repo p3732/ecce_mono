34f36f2820f799479dc282329fc7b44b189070b7
Peter <p3732@users.noreply.github.com>
2020-02-01 21:41:53 +0100

    once more state check disabling

--- src/api/client.js
+++ src/api/client.js
@@ -47,7 +47,8 @@ function getSubmissionAmount(req, res) {
 
 function voteForSubmission(req, res) {
   // check that this id can actually be voted for
-  if (/*global_state == "vote" &&*/ Number.isInteger(id) && id > 0
+  /*global_state == "vote" &&*/
+  if ( Number.isInteger(id) && id > 0
       && id <= global_votes.length) {
     global_votes[req.params.id-1] += 1;
   }
